d2904d96e5f7f6255fbd8497165c72b84701f054
patrick <mukumbu.katiwa@boldlink.io>
2022-07-12 00:14:25 +0300

    NOSTORY: change dbname

--- examples/s3-import/locals.tf
+++ examples/s3-import/locals.tf
@@ -1,6 +1,6 @@
 locals {
   name             = "mysql-import-example"
-  db_name          = "mysqlimportexample"
+  db_name          = "randomDB"
   cidr_block       = "172.16.0.0/16"
   database_subnets = cidrsubnets(local.cidr_block, 8, 8, 8)
   environment      = "examples"
